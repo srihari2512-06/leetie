c1beb72dbdd9ea583aa72e2c5c5c860df95b2f54
srihari2512-06 <sriharimohanraj2@gmail.com>
2026-08-25 08:16:09 +0000

    leetie: Add 42. Trapping Rain Water [Hard] (python3)

---
+++ solutions/Hard/0042-trapping-rain-water/solution.py
@@ -0,0 +1,38 @@
+# ──────────────────────────────────────────────────
+# Problem  : 42. Trapping Rain Water
+# Difficulty: Hard
+# Tags     : Array, Two Pointers, Dynamic Programming, Stack, Monotonic Stack
+# Link     : https://leetcode.com/problems/trapping-rain-water/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 19340000 (beats 0%)
+# Language : python3
+# Copyright: (c) 2026 srihari2512-06. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        water = 0
+        left = 0
+        right = len(height)-1
+        leftMax = 0
+        rightMax = 0
+
+        while left < right:
+            if height[left] <= height[right]:
+                if leftMax > height[left]:
+                    height[left] = leftMax
+                else:
+                    water += leftMax - height[left]
+                    leftMax = max(leftMax, height[left])
+                left += 1
+            
+            else:
+                if rightMax > height[right]:
+                    height[right] = rightMax
+                else:
+                    water += rightMax - height[right]
+                    rightMax = max(rightMax, height[right])
+                right -= 1
+        
+        return water
