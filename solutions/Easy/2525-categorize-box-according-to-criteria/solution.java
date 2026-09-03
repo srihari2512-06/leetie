9b7e3a4af43a9958527b2d0efa9876a6274bac96
srihari2512-06 <sriharimohanraj2@gmail.com>
2026-09-03 03:33:51 +0000

    leetie: Add 2525. Categorize Box According to Criteria [Easy] (java)

--- solutions/Easy/2525-categorize-box-according-to-criteria/solution.java
+++ solutions/Easy/2525-categorize-box-according-to-criteria/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Math
 // Link     : https://leetcode.com/problems/categorize-box-according-to-criteria/
 // Runtime  : 0 ms (beats 100%)
-// Memory   : 42660000 (beats 25%)
+// Memory   : 42648000 (beats 25%)
 // Language : java
 // Copyright: (c) 2026 srihari2512-06. All rights reserved.
 // Synced by: leetie
@@ -12,14 +12,18 @@
 
 class Solution {
     public String categorizeBox(int length, int width, int height, int mass) {
-        long volume = (long) length * width * height;
+        boolean bulky = length >= 10000 || width >= 10000 || height >= 10000 || (long) length * width * height >= 1000000000L;
+        boolean heavy = mass >= 100;
 
-        boolean isBulky = length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000L;
-        boolean isHeavy = mass >= 100;
-
-        if (isBulky && isHeavy) return "Both";
-        if (isBulky) return "Bulky";
-        if (isHeavy) return "Heavy";
+        if (bulky && heavy) {
+            return "Both";
+        }
+        if (bulky) {
+            return "Bulky";
+        }
+        if (heavy) {
+            return "Heavy";
+        }
         return "Neither";
     }
 }
