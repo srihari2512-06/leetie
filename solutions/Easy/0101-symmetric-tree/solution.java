74a75534ea8e5cfc5fe8aaab4c9adf533a5e4120
srihari2512-06 <srihari251206@users.noreply.github.com>
2026-09-04 02:03:40 +0000

    leetie: Add 101. Symmetric Tree [Easy] (java)

--- solutions/Easy/0101-symmetric-tree/solution.java
+++ solutions/Easy/0101-symmetric-tree/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Tree, Depth-First Search, Breadth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/symmetric-tree/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 43096000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43268000 (beats 93%)
 // Language : java
 // Copyright: (c) 2026 srihari2512-06. All rights reserved.
 // Synced by: leetie
@@ -27,10 +27,16 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root==null) return false;
-        while(root.left==root.right){
-            return true;
-        }
-        return false;
+        if(root==null) return true;
+        return isMirror(root.left,root.right);
+
+    }
+    private boolean isMirror(TreeNode t1 , TreeNode t2){
+        if(t1==null && t2==null) return true;
+        if(t1==null || t2==null) return false;
+
+        return (t1.val == t2.val)
+            && isMirror(t1.left,t2.right)
+            && isMirror(t1.right, t2.left);
     }
 }
