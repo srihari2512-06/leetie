200df357b0b8678bd3b60f88d6c40ce6c3937f11
srihari2512-06 <sriharimohanraj2@gmail.com>
2026-09-17 06:33:20 +0000

    leetie: Add 1752. Check if Array Is Sorted and Rotated [Easy] (java)

--- solutions/Easy/1752-check-if-array-is-sorted-and-rotated/solution.java
+++ solutions/Easy/1752-check-if-array-is-sorted-and-rotated/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array
 // Link     : https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
-// Runtime  : 0 ms (beats 100%)
-// Memory   : 43376000 (beats 12%)
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42592000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 srihari2512-06. All rights reserved.
 // Synced by: leetie
@@ -13,11 +13,14 @@
 class Solution {
     public boolean check(int[] nums) {
         int count = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>nums[(i+1)%nums.length]){
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
                 count++;
             }
         }
+         if(nums[nums.length-1]>nums[0]){
+                count++;
+            }
         return count<=1;
     }
 }
