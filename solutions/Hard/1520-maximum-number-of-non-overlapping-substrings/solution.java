667c5eeab0521224e0db3c33b17618c307e447dc
srihari2512-06 <srihari251206@users.noreply.github.com>
2026-09-18 02:58:16 +0000

    leetie: Add 1520. Maximum Number of Non-Overlapping Substrings [Hard] (java)

--- solutions/Hard/1520-maximum-number-of-non-overlapping-substrings/solution.java
+++ solutions/Hard/1520-maximum-number-of-non-overlapping-substrings/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Hash Table, String, Greedy, Sorting
 // Link     : https://leetcode.com/problems/maximum-number-of-non-overlapping-substrings/
 // Runtime  : 8 ms (beats 96%)
-// Memory   : 48220000 (beats 36%)
+// Memory   : 47604000 (beats 97%)
 // Language : java
 // Copyright: (c) 2026 srihari2512-06. All rights reserved.
 // Synced by: leetie
